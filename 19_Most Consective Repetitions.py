124768d8e981bfaafe56f91ea4acf937898ba37d
hira ijaz <68460038+HiraIjaz@users.noreply.github.com>
2022-08-16 18:49:53 +0500

    Most Consecutive Repetitons

---
+++ 19_Most Consective Repetitions.py
@@ -0,0 +1,43 @@
+# coding: utf-8
+
+
+# Longest Repetition
+
+# Define a procedure, longest_repetition, that takes as input a 
+# list, and returns the element in the list that has the most 
+# consecutive repetitions. If there are multiple elements that 
+# have the same number of longest repetitions, the result should 
+# be the one that appears first. If the input list is empty, 
+# it should return None.
+
+def longest_repetition(li):
+  if len(li)==0: return 
+  currEle=li[1]
+  currCount=0
+  longestCount=0
+  longestEle=li[1]
+  for x in li:
+    if currCount>longestCount:
+      longestCount=currCount
+      longestEle=currEle
+
+    if x==currEle:
+      currCount+=1
+    else:
+      currEle=x
+      currCount=1
+  return longestEle
+
+# For example,
+
+print longest_repetition([1, 2, 2, 3, 3, 3, 2, 2, 1])
+# 3
+
+print longest_repetition(['a', 'b', 'b', 'b', 'c', 'd', 'd', 'd'])
+# b
+
+print longest_repetition([1, 2, 3, 4, 5])
+# 1
+
+print longest_repetition([])
+# None
