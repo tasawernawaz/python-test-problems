f2a4aa480286dbeadef5327b003663ceb29bacdc
hira ijaz <68460038+HiraIjaz@users.noreply.github.com>
2022-08-17 23:11:13 +0500

    Update 2_Last Position & Math Table.py

--- 2_Last Position & Math Table.py
+++ 2_Last Position & Math Table.py
@@ -6,9 +6,11 @@
 # input number. The order in which the equations are printed matters.
 
 def print_multiplication_table(n):
-    for i in range(1,n+1):
-      for j in range(1,n+1):
-        print(i,' * ',j," = ",i*j)
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            print (i, ' * ', j, ' = ', i * j)
+
+        
 print_multiplication_table(3)
 
 # print_multiplication_table(2)
