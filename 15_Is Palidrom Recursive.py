d719ab46f195798d8443b41b0d953b3a591b74de
hira ijaz <68460038+HiraIjaz@users.noreply.github.com>
2022-08-26 17:12:09 +0500

    Update 15_Is Palidrom Recursive.py

--- 15_Is Palidrom Recursive.py
+++ 15_Is Palidrom Recursive.py
@@ -8,17 +8,16 @@
 # if they do match, is the middle a palindrome?
 
 def is_palindrome(s):
-
-
     if len(s) < 2:
         return True
     if s[0] != s[-1]:
         return False
     return is_palindrome(s[1:-1])
 
-print is_palindrome('')
+
+print(is_palindrome(''))
 # >>> True
-print is_palindrome('abab')
+print(is_palindrome('abab'))
 # >>> False
-print is_palindrome('abba')
+print(is_palindrome('abba'))
 # >>> True
