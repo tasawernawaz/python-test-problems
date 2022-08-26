cb9ef1115ecf91d3ed5548226d03f13aa277eac8
hira ijaz <68460038+HiraIjaz@users.noreply.github.com>
2022-08-26 17:29:56 +0500

    Update 10_Download Time Calculator.py

--- 10_Download Time Calculator.py
+++ 10_Download Time Calculator.py
@@ -2,7 +2,7 @@
 
 # Write a procedure download_time which takes as inputs a file size, the
 # units that file size is given in, bandwidth and the units for
-# bandwidth (excluding per second) and returns the time taken to download 
+# bandwidth (excluding per second) and returns the time taken to download
 # the file.
 # Your answer should be a string in the form
 # "<number> hours, <number> minutes, <number> seconds"
@@ -22,8 +22,7 @@
 
 # print 2 ** 40      # one terabit, Tb
 # print 2 ** 40 * 8  # one terabyte, TB
-
-# Often bandwidth is given in megabits (Mb) per second whereas file size 
+# Often bandwidth is given in megabits (Mb) per second whereas file size
 # is given in megabytes (MB).
 
 def convert_seconds(number):
@@ -31,16 +30,14 @@ def convert_seconds(number):
     hrs = int(number / 3600)
     mins = int((number - hrs * 3600) / 60)
     secs = int(number - hrs * 3600 - mins * 60)
-    
-    time = time + str(secs) + ' hour ' if hrss == 1 else ' hourss'
-    time = time + str(secs) + ' minute ' if mins == 1 else ' minutes'
-    time = time + str(secs) + ' second ' if secs == 1 else ' seconds'
-    
+    time = time + str(secs) + ' hour ' if hrs == 1 else 'hours'
+    time = time + str(secs) + ' minute ' if mins == 1 else 'minutes'
+    time = time + str(secs) + ' second ' if secs == 1 else 'seconds'
+
     return time
 
 
-def download_time(filesize, fileUnit, BandWidth, BandWiddthUnit,):
-    
+def download_time(filesize, fileUnit, BandWidth, BandWiddthUnit, ):
     if fileUnit == 'kb':
         filesize *= 2 ** 10
     elif fileUnit == 'kB':
@@ -76,7 +73,6 @@ def download_time(filesize, fileUnit, BandWidth, BandWiddthUnit,):
         BandWidth *= 2 ** 40 * 8
 
     return convert_seconds(filesize / BandWidth)
-    
 
 
 print download_time(1024, 'kB', 1, 'MB')
@@ -92,7 +88,9 @@ print download_time(13, 'GB', 5.6, 'Mb')
 # >>> 5 hours, 16 minutes, 57.1428571429 seconds
 
 print download_time(10, 'MB', 2, 'kB')
-# >>> 1 hour, 25 minutes, 20 seconds  # 20.0 seconds is also acceptable
+# >>> 1 hour, 25 minutes, 20 seconds  
+# 20.0 seconds is also acceptable
 
-print download_time(10, 'MB', 2, 'kb')
+print
+download_time(10, 'MB', 2, 'kb')
 # >>> 11 hours, 22 minutes, 40 seconds  # 40.0 seconds is also acceptable
