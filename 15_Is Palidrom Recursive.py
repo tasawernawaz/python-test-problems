e31f860b69e2d0fbf2100b1f10c521d4a5e55c01
hira ijaz <68460038+HiraIjaz@users.noreply.github.com>
2022-08-26 17:23:27 +0500

    Update 15_Is Palidrom Recursive.py

--- 15_Is Palidrom Recursive.py
+++ 15_Is Palidrom Recursive.py
@@ -1,11 +1,3 @@
-# Define a recursive procedure is_palindrome, that takes as input a 
-# string, and returns a
-# Boolean indicating if the input string is a palindrome.
-
-# Base Case: '' => True
-# Recursive Case: if first and last characters don't match => False
-# if they do match, is the middle a palindrome?
-
 def is_palindrome(s):
     if len(s) < 2:
         return True
