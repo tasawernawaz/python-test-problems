fa18352d210d6f8c4591539fd7e2d34b291f34a0
hira ijaz <68460038+HiraIjaz@users.noreply.github.com>
2022-08-26 18:00:27 +0500

    Update 2_Last Position & Math Table.py

--- 2_Last Position & Math Table.py
+++ 2_Last Position & Math Table.py
@@ -9,8 +9,8 @@ def print_multiplication_table(n):
     for i in range(1, n + 1):
         for j in range(1, n + 1):
             print(i, ' * ', j, ' = ', i * j)
-            
-            
+
+
 print_multiplication_table(3)
 
 # print_multiplication_table(2)
