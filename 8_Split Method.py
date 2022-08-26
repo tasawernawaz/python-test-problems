9eb06a3b8271ef3373cc6f7ff6abe30967922968
hira ijaz <68460038+HiraIjaz@users.noreply.github.com>
2022-08-26 17:36:53 +0500

    Update 8_Split Method.py

--- 8_Split Method.py
+++ 8_Split Method.py
@@ -29,15 +29,4 @@ def split_string(source, splitlist):
 
 out = split_string('This is a test-of the,string separation-code!',
                    ' ,!-')
-print out
-
-# >>> ['This', 'is', 'a', 'test', 'of', 'the', 'string', 'separation', 'code']
-
-# out = split_string("After  the flood   ...  all the colors came out.", " .")
-# print out
-# >>> ['After', 'the', 'flood', 'all', 'the', 'colors', 'came', 'out']
-
-# out = split_string("First Name,Last Name,
-Street Address,City,State,Zip Code",",")
-# print out
-# >>>['First Name', 'Last Name', 'Street Address', 'City', 'State', 'Zip Code']
+print(out)
