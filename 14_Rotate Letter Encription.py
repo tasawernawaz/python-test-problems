9efeeaacc141fe630617b0f11e1ff80331a0f2a4
hira ijaz <68460038+HiraIjaz@users.noreply.github.com>
2022-08-26 17:06:32 +0500

    Update 14_Rotate Letter Encription.py

--- 14_Rotate Letter Encription.py
+++ 14_Rotate Letter Encription.py
@@ -1,9 +1,12 @@
 # coding: utf-8
 
 # Write a procedure, rotate which takes as its input a string of lower case
-# letters, a-z, and spaces, and an integer n, and returns the string constructed
-# by shifting each of the letters n steps, and leaving the spaces unchanged.
-# Note that 'a' follows 'z'. You can use an additional procedure if you
+# letters, a-z, and spaces, and an integer n,
+# and returns the string constructed
+# by shifting each of the letters n steps, and 
+# leaving the spaces unchanged.
+# Note that 'a' follows 'z'. You can use an 
+# additional procedure if you
 # choose to as long as rotate returns the correct string.
 # Note that n can be positive, negative or zero.
 
@@ -29,15 +32,14 @@ def rotate(string, number):
     return rotatedstring
 
 
-
-print rotate('sarah', 13)
+print(rotate('sarah', 13))
 # >>> 'fnenu'
-print rotate('fnenu', 13)
+print(rotate('fnenu', 13))
 # >>> 'sarah'
-print rotate('dave', 5)
+print(rotate('dave', 5))
 # >>>'ifaj'
-print rotate('ifaj', -5)
+print(rotate('ifaj', -5))
 # >>>'dave'
-print rotate(("zw pfli tfuv nfibj tfiivtkcp pfl jyflcu "
-              "sv rscv kf ivru kyzj"), -17)
+print(rotate(("zw pfli tfuv nfibj tfiivtkcp pfl jyflcu "
+              "sv rscv kf ivru kyzj"), -17))
 # >>> if your code works correctly you should be able to read this
