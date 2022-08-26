3044aa290c89d5332037a0682d2f0f6d9bac4de1
hira ijaz <68460038+HiraIjaz@users.noreply.github.com>
2022-08-26 17:38:49 +0500

    Update 7_Crypto Analysis.py

--- 7_Crypto Analysis.py
+++ 7_Crypto Analysis.py
@@ -2,14 +2,19 @@
 
 # Crypto Analysis: Frequency Analysis
 #
-# To analyze encrypted messages, to find out information about the possible 
-# algorithm or even language of the clear text message, one could perform 
-# frequency analysis. This process could be described as simply counting 
+# To analyze encrypted messages,
+# to find out information about the possible 
+# algorithm or even language of the clear text message,
+# one could perform 
+# frequency analysis. This process could be
+# described as simply counting 
 # the number of times a certain symbol occurs in the given text. 
 # For example:
-# For the text "test" the frequency of 'e' is 1, 's' is 1 and 't' is 2.
+# For the text "test" the frequency of
+# 'e' is 1, 's' is 1 and 't' is 2.
 #
-# The input to the function will be an encrypted body of text that only contains 
+# The input to the function will be an
+# encrypted body of text that only contains 
 # the lowercase letters a-z. 
 # As output you should return a list of the normalized frequency 
 # for each of the letters a-z. 
